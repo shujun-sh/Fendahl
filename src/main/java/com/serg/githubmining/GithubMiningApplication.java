0df62c0afc3c58319230d96aeb164b0589fe4c60
Mattia Bonfanti <bonfantimattia@gmail.com>
2023-12-01 12:41:22 +0100

    Add resources file
    - Add resources file

--- src/main/java/com/serg/githubmining/GithubMiningApplication.java
+++ src/main/java/com/serg/githubmining/GithubMiningApplication.java
@@ -2,11 +2,15 @@ package com.serg.githubmining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class GithubMiningApplication {
 
 	public static void main(String[] args) {
+		// Load the properties.
+		System.setProperty("spring.config.name", "github-mining");
 		SpringApplication.run(GithubMiningApplication.class, args);
 	}
 
