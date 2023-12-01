09dfe004810588fa3f552b89a6cc8e7bbc73ac99
Mattia Bonfanti <bonfantimattia@gmail.com>
2023-12-01 12:13:21 +0100

    Setup MVN project
    - Setup MVN project

---
+++ src/main/java/com/serg/githubmining/GithubMiningApplication.java
@@ -0,0 +1,13 @@
+package com.serg.githubmining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubMiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubMiningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serg/githubmining/GithubMiningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serg.githubmining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubMiningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
